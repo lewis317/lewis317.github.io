0ab48431aa2d2c1b9ceaf05b1f62f9617b70cb1b
lewis317 <46941855+lewis317@users.noreply.github.com>
2019-02-19 01:28:29 -0700

    Debugging

--- javascript/mitchell.js
+++ javascript/mitchell.js
@@ -1,204 +1,205 @@
 require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
 	const map = new Map({
-		basemap: "streets"
+		basemap: "osm"
 	});
 	
 	const view = new MapView({
 		container: "viewDiv",
 		map: map,
-		zoom: 12,
+		zoom: 5,
 		center: {
-			latitude: 39.828240
+			latitude: 39.828240,
 			longitude: -98.579471
 		}
 	});
-});
 
 
-//Mcgee Poster
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+
+	//Mcgee Poster
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 40.768839,
+			longitude: -111.901739
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 40.768839,
-		longitude: -111.901739,
-	}
-});
+	});
 
 
-//Christmas Gift
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+	//Christmas Gift
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 40.767787,
+			longitude: -111.900395
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 40.767787,
-		longitude: -111.900395,
-	}
-});
+	});
 
 
-//Welcome to SLC BBB
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+	//Welcome to SLC BBB
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 40.768309,
+			longitude: -111.901103
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 40.768309,
-		longitude: -111.901103,
-	}
-});
+	});
 
 
-//Dunkic
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+	//Dunkic
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 40.767712,
+			longitude: -111.901990
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 40.767712,
-		longitude: -111.901990,
-	}
-});
+	});
 
 
-//0-8 in SLC
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+	//0-8 in SLC
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 40.768900,
+			longitude: -111.900288
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 40.768900,
-		longitude: -111.900288,
-	}
-});
+	});
 
 
-//Dunk Contest Champ
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+	//Dunk Contest Champ
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 34.043044,
+			longitude: -118.267426
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 34.043044,
-		longitude: -118.267426,
-	}
-});
+	});
 
 
-//Playoff Anouncement
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+	//Playoff Anouncement
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 29.750732,
+			longitude: -95.362058
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 29.750732,
-		longitude: -95.362058,
-	}
-});
+	});
 
 
-//Miami Vices
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+	//Miami Vices
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 25.781326,
+			longitude: -80.187867
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 25.781326,
-		longitude: -80.187867,
-	}
-});
+	});
 
 
-//Miles High
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+	//Miles High
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 39.749288,
+			longitude: -105.007415
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 39.749288,
-		longitude: -105.007415,
-	}
-});
+	});
 
-//College Days
-view.graphics.add({
-	symbol:{
-		type:"text",
-		color: "#070428",
-		text: "\ue61e",
-		font: {
-			size: 30,
-			family: "CalciteWebCoreIcons"
+	//College Days
+	view.graphics.add({
+		symbol:{
+			type:"text",
+			color: "#070428",
+			text: "\ue61e",
+			font: {
+				size: 30,
+				family: "CalciteWebCoreIcons"
+			}
+		},
+		geometry: {
+			type: "point",
+			latitude: 38.257714,
+			longitude: -85.753539
 		}
-	},
-	geometry: {
-		type: "point",
-		latitude: 38.257714,
-		longitude: -85.753539,
-	}
+	});
 });
